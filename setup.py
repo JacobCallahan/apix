52914ee16355b2580580d3da288a06fec7d766a0
Jacob Callahan <jacob.callahan05@gmail.com>
2018-02-18 23:30:43 -0500

    Added initial tests and Travis Added 21 initial tests Enabled apix's test subcommand Added Travis CI config

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name='apix',
-    version='0.2.1',
+    version='0.2.2',
     description="API Explorer: Discover, Track, Test.",
     long_description=readme + '\n\n' + history,
     author="Jacob J Callahan",
