1267e83c4de2aaeb3c8fa86cd02bca5bb8ee6279
Jacob Callahan <jacob.callahan05@gmail.com>
2019-07-06 21:46:27 -0400

    Adding three new templates These new templates provide basic, intermediate, and advanced functionality

--- apix/libtools/libmaker.py
+++ apix/libtools/libmaker.py
@@ -4,13 +4,21 @@ import attr
 from pathlib import Path
 from logzero import logger
 from apix import helpers
-from apix.libtools import nailgun
+from apix.libtools import advanced, basic, intermediate, nailgun
+
+TEMPLATE_MAKERS = {
+    "advanced": advanced.AdvancedMaker,
+    "basic": basic.BasicMaker,
+    "intermediate": intermediate.IntermediateMaker,
+    "nailgun": nailgun.NailgunMaker,
+}
 
 
 @attr.s()
 class LibMaker:
     api_name = attr.ib(default=None)
     api_version = attr.ib(default=None)
+    template_name = attr.ib(default=None)
 
     def __attrs_post_init__(self):
         if not self.api_name:
@@ -25,15 +33,14 @@ class LibMaker:
             self.api_version = helpers.get_latest(self.api_name)
 
     def make_lib(self):
-        if self.api_name.lower() == "satellite6":
-            logger.info(
-                f"Making a nailgun library for {self.api_version}"
-                " at libs/generated/nailgun/"
-            )
-            api_dict = helpers.load_api(self.api_name, self.api_version)
-            nailgun_maker = nailgun.NailgunMaker(
-                api_dict=api_dict, api_name=self.api_name, api_version=self.api_version
-            )
-            nailgun_maker.make()
-        else:
+        TemplateMaker = TEMPLATE_MAKERS.get(self.template_name.lower())
+        if not TemplateMaker:
             logger.warning(f"I don't know how to make a library for {self.api_name}")
+            return
+        logger.info(f"Making a {self.template_name} library for {self.api_version}")
+        api_dict = helpers.load_api(self.api_name, self.api_version)
+        lib_maker = TemplateMaker(
+            api_dict=api_dict, api_name=self.api_name, api_version=self.api_version
+        )
+        lib_maker.make()
+
